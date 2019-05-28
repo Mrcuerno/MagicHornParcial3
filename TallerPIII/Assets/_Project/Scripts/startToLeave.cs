c9e8c763b3643738c8d39100f8f18fbfda904e5f
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-05-28 15:46:55 -0500

    FerCambios

---
+++ TallerPIII/Assets/_Project/Scripts/startToLeave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class startToLeave : MonoBehaviour
+{
+
+    private bool sfx;
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape)|| Input.GetButton("Start"))
+        {
+            if (!sfx)
+            {
+                //AudioSource.PlayClipAtPoint(s, Vector3.zero);
+                Invoke("LoadScene", 0.5f);
+                sfx = true;
+            }
+        }
+
+    }
+
+    void LoadScene()
+    {
+        Application.LoadLevel("Nivel1");
+    }
+}
---
+++ TallerPIII/Assets/_Project/Scripts/startToPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class startToPlay : MonoBehaviour
+{
+    public AudioClip s;
+    private bool sfx;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.anyKeyDown)
+        {
+            if (!sfx)
+            {
+                AudioSource.PlayClipAtPoint(s, Vector3.zero);
+                Invoke("LoadScene", 0.5f);
+                sfx = true;
+            }
+        }
+
+    }
+
+    void LoadScene()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
