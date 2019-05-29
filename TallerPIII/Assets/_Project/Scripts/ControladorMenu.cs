5d9a1c8eb05b3cea4620cad4f57734d5eb3e1c0c
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-05-28 20:13:52 -0500

    XD

---
+++ TallerPIII/Assets/_Project/Scripts/ControladorMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ControladorMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CambiarEsecena(string nombre)
+    {
+        print("Cambiando a la escena " + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    
+}
