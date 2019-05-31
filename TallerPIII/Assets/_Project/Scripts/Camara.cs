cd64481d57e07dc8adce6b9de807f539085c0e89
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-05-31 12:52:18 -0500

    1.5 jump

---
+++ TallerPIII/Assets/_Project/Scripts/Camara.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed;
+
+    public Vector3 offset;
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+
+        Vector3 desirePosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desirePosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+
+       
+    }
+}
