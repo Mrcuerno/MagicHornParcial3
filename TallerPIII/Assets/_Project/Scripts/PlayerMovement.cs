e1e54c89d7fa2b51eb4660860a1d726269932cdf
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-06-06 20:45:26 -0500

    ?

--- TallerPIII/Assets/_Project/Scripts/PlayerMovement.cs
+++ TallerPIII/Assets/_Project/Scripts/PlayerMovement.cs
@@ -1,31 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using EZCameraShake;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
-
     public Rigidbody2D rigi;
-
     public float runSpeed = 40f;
-    public Camara cam;
     float horizontalMove = 0f;
     bool jump = false;
     public int jumpa = 100;
     bool crouch = false;
     bool tocarpared = false;
-    bool tocarI = false;
-    bool tocarD = false;
 
-    void CaerPared()
-    {
-        //GetComponent<BoxCollider2D>().sharedMaterial = null;
-        //GetComponent<CircleCollider2D>().sharedMaterial = null;
-        Debug.Log("Entroxx");
-    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -39,65 +30,56 @@ public class PlayerMovement : MonoBehaviour
             rigi.constraints = RigidbodyConstraints2D.FreezeRotation;
             rigi.gravityScale = 3;
             StopAllCoroutines();
-            controller.Jumpu(jumpa);
-
-            
-            
+            controller.Jumpu(jumpa); 
         }
-
         if (Input.GetButtonDown("Crouch")   /*||Input.GetAxis("Vertical") < 0*/)
         {
             crouch = true;
-
         }
         else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
         }
-
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Colisionando");
-
-
+        //Debug.Log("Colisionando");
         if (collision.gameObject.CompareTag("Pared") && jump)
         {
-            if (collision.gameObject.transform.position.x < this.transform.position.x)
+           if (collision.gameObject.transform.position.x < this.transform.position.x)
             {
+                Debug.Log("Pared Izq");
 
+                //this.transform.localScale = new Vector3(1.0f, 0, 0);
 
             }
 
+            if (collision.gameObject.transform.position.x > this.transform.position.x)
+            {
+                Debug.Log("Pared Derecha");
+                //this.transform.localScale = new Vector3(1.0f, 0, 0);
+
+            }
             tocarpared = true;
             jump = true;
-
-            Debug.Log("El brinco" + jump);
-            Debug.Log(tocarpared);
+            //Debug.Log("El brinco" + jump);
+            //Debug.Log(tocarpared);
             rigi.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
             StartCoroutine(Fernando());
             rigi.gravityScale = 0;
             rigi.velocity = Vector2.zero;
-            CaerPared();
         }
-
         jump = false;
     }
-
-    
-
-        void FixedUpdate()
+    void FixedUpdate()
     {
-        // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        
     }
 
     public IEnumerator Fernando()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.5f);
         rigi.constraints = RigidbodyConstraints2D.FreezeRotation;
         rigi.gravityScale = 3;
     }
